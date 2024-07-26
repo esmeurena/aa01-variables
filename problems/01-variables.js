873c5f0b078f6e004a177db23a7a533943003511
esmeurena <esme.urena27@gmail.com>
2024-07-25 20:40:16 -0400

    variables-practice-finished-untested

--- problems/01-variables.js
+++ problems/01-variables.js
@@ -8,34 +8,44 @@ test your work by running your code and the test specs!
 
 // Your code here 
 
+let firstName = "Esmeralda";
 
 // 2. Declare a variable called `lastName` and assign it your last name
 //    as a string:
 
 // Your code here 
 
+let lastName = "Urena";
 
 // 3. Declare a variable called `age`:
 
 // Your code here 
 
+let age;
 
 // 4. Print out the `firstName`, `lastName`, and `age` variables. What
-//    do you expect them to be when they get printed?
+//    do you expect them to be when they get printed? Esmeralda\n Urena\n undefined
 
 // Your code here 
 
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 
 // 5. Assign the `age` variable to a number:
 
 // Your code here 
 
+age = 25;
 
 // 6. Print out the `firstName`, `lastName`, and `age` variables. What
-//    do you expect them to be when they get printed?
+//    do you expect them to be when they get printed? Esmeralda\n Urena\n 25
 
 // Your code here 
 
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 const obj = {};
